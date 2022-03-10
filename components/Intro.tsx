0038cdf06156774d07fd07c7536b9c2e6398a3df
lawrencelu5 <lawrencelu18@gmail.com>
2022-03-09 19:45:48 -0800

    inital commit

---
+++ components/Intro.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+      <p className="font-serif text-2xl">Hi, my name is</p>
+      <h1 className="font-serif text-7xl">Lawrence Lu</h1>
+    </div>
+  )
+}
+
+export default Intro
