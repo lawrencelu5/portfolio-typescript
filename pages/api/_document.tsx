0022810b611884b434608f22b2a0c946326a71c6
lawrencelu5 <lawrencelu18@gmail.com>
2022-04-10 20:29:24 -0700

    head title

---
+++ pages/api/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Lawrence Lu</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="keywords"
+          content="Lawrence, Lu, Lawrence Lu, web dev, developer, programmer"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
